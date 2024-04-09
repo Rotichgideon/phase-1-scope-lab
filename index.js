cfdffd87566cc247003da2a06306bcf11b9a7be0
gideon rotich <gideonrotich2001@gmail.com>
2024-04-09 22:11:07 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer () {
+    var bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer () {
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "kijanaa";
+
+function changeLeastFavoriteCustomer () {
+    return leastFavoriteCustomer = "we mzee!"
+}
+
+upperCaseCustomerName();
+setBestCustomer();
+overwriteBestCustomer();
+changeLeastFavoriteCustomer();
